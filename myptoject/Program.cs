1def43b6d6c1b442843c0de351d2c880692ca845
GargylBoom <supertrek21@gmail.com>
2021-07-13 03:27:15 +0300

    megaladon

---
+++ myptoject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Никита Гвоздев" +
+                               "\n30.09.2000" +
+                               "\n@Gargylboom" + 
+                               "\n+380957230785");
+            Console.ReadLine();
+
+        }
+    }
+}
